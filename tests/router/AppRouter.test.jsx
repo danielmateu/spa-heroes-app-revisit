16a583cc55021ec1b8cbbe04844aff8e23680e36
Daniel Mateu <danielmateu86@gmail.com>
2023-03-21 13:42:19 +0100

    Tests sobre navbar

--- tests/router/AppRouter.test.jsx
+++ tests/router/AppRouter.test.jsx
@@ -1,7 +1,52 @@
+import { AuthContext } from "../../src/auth/context/AuthContext"
+import { AppRouter } from "../../src/router/AppRouter"
+import { render, screen } from "@testing-library/react"
+import { MemoryRouter } from "react-router-dom"
+
 describe('Pruebas sobre AppRouter', () => {
 
-    test('', () => {
+    test('Debe de mostrar el login si no está autenticado', () => {
+        const contextValue = {
+            dispatch: jest.fn(),
+            user: {
+                logged: false
+            }
+        }
 
+        render(
+            <MemoryRouter initialEntries={['/marvel']}>
+                <AuthContext.Provider value={contextValue}>
+                    <AppRouter />
+                </AuthContext.Provider>
+            </MemoryRouter>
+        )
+
+        // screen.debug()
+        expect(screen.getAllByText('Login').length).toBe(1)
+    })
+
+    test('Debe de mostrar el componente marvel si está autenticado', () => {
+        const contextValue = {
+            // dispatch: jest.fn(),
+            user: {
+                logged: true,
+                user: {
+                    name: 'Juan',
+                    id: '123'
+                }
+            }
+        }
+
+        render(
+            <MemoryRouter initialEntries={['/login']}>
+                <AuthContext.Provider value={contextValue}>
+                    <AppRouter />
+                </AuthContext.Provider>
+            </MemoryRouter>
+        )
+
+        // screen.debug()
+        // expect(screen.getAllByText('Marvel').length).toBeGreaterThan(1)
     })
 
 
---
+++ tests/ui/components/Navbar.test.jsx
@@ -0,0 +1,73 @@
+import { render, screen } from "@testing-library/react"
+import { MemoryRouter } from "react-router-dom"
+import { AuthContext } from "../../../src/auth/context/AuthContext"
+import { Navbar } from "../../../src/ui/components/Navbar"
+
+describe('Pruebas sobre Navbar', () => {
+
+    const contextValue = {
+        user: {
+            name: 'Fernando',
+            logged: true
+        },
+        logout: jest.fn()
+    }
+
+    beforeEach(() => {
+        jest.clearAllMocks()
+    })
+
+    test('Debe de mostrarse el componente correctamente', () => {
+
+        render(
+            <MemoryRouter>
+                <AuthContext.Provider value={contextValue}>
+                    <Navbar />
+                </AuthContext.Provider>
+            </MemoryRouter>
+        )
+        
+        // screen.debug()
+        expect(screen.getByText('Comics App')).toBeTruthy()
+        expect(screen.getByText('Marvel')).toBeTruthy()
+        expect(screen.getByText('DC')).toBeTruthy()
+        expect(screen.getByText('Logout')).toBeTruthy()
+
+    })
+
+    test('Debe de mostrar el nombre del usuario logueado', () => {
+
+        render(
+            <MemoryRouter>
+                <AuthContext.Provider value={contextValue}>
+                    <Navbar />
+                </AuthContext.Provider>
+            </MemoryRouter>
+        )
+
+        expect(screen.getByText('Fernando')).toBeTruthy()
+    })
+
+    test('Debe de llamar el logout y el useNavigate', () => {
+
+        
+
+        const historyMock = {
+            replace: jest.fn()
+        }
+
+        render(
+            <MemoryRouter>
+                <AuthContext.Provider value={contextValue}>
+                    <Navbar />
+                </AuthContext.Provider>
+            </MemoryRouter>
+        )
+
+        const logoutButton = screen.getByText('Logout')
+        logoutButton.click()
+
+        expect(contextValue.logout).toHaveBeenCalled()
+        // expect(historyMock.replace).toHaveBeenCalledWith('/login')
+    })
+})
