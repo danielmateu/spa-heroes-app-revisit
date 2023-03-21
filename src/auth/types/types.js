e82a2c136f9c1c34c4df5a16915f130a4cdd8cd7
Daniel Mateu <danielmateu86@gmail.com>
2023-03-21 12:15:53 +0100

    Tests sobre types

--- src/auth/types/types.js
+++ src/auth/types/types.js
@@ -3,10 +3,5 @@
 export const types = {
     login: '[Auth] Login',
     logout: '[Auth] Logout',
-    // uiSetError: '[UI] Set Error',
-    // uiRemoveError: '[UI] Remove Error',
-    // uiStartLoading: '[UI] Start loading',
-    // uiFinishLoading: '[UI] Finish loading',
-    // notesAddNew: '[Notes] New note',
-    // notesActive: '[Notes] Set active note',
+
 }
